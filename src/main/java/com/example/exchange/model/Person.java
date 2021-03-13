2e4a6e1b9adb33637961d570678d863696f87646
Hatef Zamany <hatef.zamany@yahoo.com>
2021-03-13 12:39:17 +0330

    add Person class

---
+++ src/main/java/com/example/exchange/model/Person.java
@@ -0,0 +1,31 @@
+package com.example.exchange.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.ArrayList;
+
+public class Person {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    int id;
+    ArrayList<Product> products;
+
+    public Person(int id, ArrayList<Product> products) {
+        this.id = id;
+        this.products = products;
+    }
+
+    public ArrayList<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(ArrayList<Product> products) {
+        this.products = products;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
