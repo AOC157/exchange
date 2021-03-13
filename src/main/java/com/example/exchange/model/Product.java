f4a3a53826d781ed120881e8f87718b9b484cbac
Hatef Zamany <hatef.zamany@yahoo.com>
2021-03-13 12:28:19 +0330

    add Product class

---
+++ src/main/java/com/example/exchange/model/Product.java
@@ -0,0 +1,96 @@
+package com.example.exchange.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+public class Product {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private int id;
+    @NotNull(message = "name can't be null")
+    @Size(min = 1, max = 200)
+    private String name;
+    @NotNull(message = "color cannot be null")
+    @Size(min = 1, max = 200)
+    private String color;
+    @NotNull(message = "price cannot be null")
+    @Min(value = 1)
+    private double price;
+    @NotNull(message = "date of manufacture cannot be null")
+    private Date dateOfManufacture;
+    private Date expirationDate;
+
+
+    public Product(String name, String color, double price, Date dateOfManufacture, Date expirationDate) {
+        this.name = name;
+        this.color = color;
+        this.price = price;
+        this.dateOfManufacture = dateOfManufacture;
+        this.expirationDate = expirationDate;
+    }
+
+    public Product(){
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Date getDateOfManufacture() {
+        return dateOfManufacture;
+    }
+
+    public void setDateOfManufacture(Date dateOfManufacture) {
+        this.dateOfManufacture = dateOfManufacture;
+    }
+
+    public Date getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(Date expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "\"id\":" + id +
+                ", \"name\":\"" + name + '\"' +
+                ", \"color\":\"" + color + '\"' +
+                ", \"price\":" + price +
+                ", \"dateOfManufacture\":\"" + dateOfManufacture + '\"' +
+                ", \"expirationDate\":\"" + expirationDate + "\"" +
+                '}';
+    }
+}
