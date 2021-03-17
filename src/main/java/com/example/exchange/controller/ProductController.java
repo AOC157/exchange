5d0f0c9295fc6ac2615ee813dc2aea9bd42df898
Hatef Zamany <hatef.zamany@yahoo.com>
2021-03-17 15:50:33 +0330

    add ProductController

---
+++ src/main/java/com/example/exchange/controller/ProductController.java
@@ -0,0 +1,36 @@
+package com.example.exchange.controller;
+
+import com.example.exchange.model.Product;
+import com.example.exchange.service.ProductService;
+import javassist.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+@Validated
+public class ProductController {
+
+    @Autowired
+    public ProductService productService;
+
+
+    @PostMapping(value = "/insert")
+    public void insertProduct(@Valid @RequestBody Product product) {
+        productService.save(product);
+    }
+
+    @GetMapping(value = "/getAll")
+    public List<Product> getAllProducts() {
+        return productService.getAll();
+    }
+
+    @GetMapping(value = "/get/{id}")
+    public String getOneProduct(@PathVariable("id") int id) throws NotFoundException {
+        return productService.get(id).toString();
+    }
+}
