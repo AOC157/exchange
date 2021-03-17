47881205ac54955b94aa9a519eea1d951a625612
Hatef Zamany <hatef.zamany@yahoo.com>
2021-03-17 20:25:03 +0330

    add PersonController

---
+++ src/main/java/com/example/exchange/controller/PersonController.java
@@ -0,0 +1,35 @@
+package com.example.exchange.controller;
+
+import com.example.exchange.model.Person;
+import com.example.exchange.service.PersonService;
+import javassist.NotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+@Validated
+public class PersonController {
+
+    @Autowired
+    public PersonService personService;
+
+    @PostMapping(value = "/insert")
+    public void insertPerson(@Valid @RequestBody Person person) {
+        personService.save(person);
+    }
+
+    @GetMapping(value = "/getAll")
+    public List<Person> getAllPeople() {
+        return personService.getAll();
+    }
+
+    @GetMapping(value = "/get/{id}")
+    public String getOnePerson(@PathVariable("id") int id) throws NotFoundException {
+        return personService.get(id).toString();
+    }
+}
