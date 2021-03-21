127ccb1db4a7a1d901d9fbc04b7353658cdf0191
Hatef Zamany <hatef.zamany@yahoo.com>
2021-03-21 21:23:59 +0330

    add test to check validation of person fields

---
+++ src/test/java/com/example/exchange/model/PersonTest.java
@@ -0,0 +1,48 @@
+package com.example.exchange.model;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Arrays;
+import java.util.Collections;
+
+@RunWith(SpringRunner.class)
+public class PersonTest {
+
+    private Validator validator;
+
+    @Before
+    public void setUp(){
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test(expected = AssertionError.class)
+    public void personValidationTest() {
+        Product product1 = new Product("cake","brown",5000.0);
+        Product product2 = new Product("pen","red",7000.0);
+
+        Person person1 = new Person(Arrays.asList(product1,product2));
+
+        Person person2 = new Person(Collections.singletonList(product1));
+
+        assert(validator.validate(person2).isEmpty());
+    }
+
+    @Test
+    public void personValidationTest1() {
+        Product product1 = new Product("cake","brown",5000.0);
+        Product product2 = new Product("pen","red",7000.0);
+
+        Person person1 = new Person(Collections.singletonList(product1));
+
+        Person person2 = new Person(Collections.singletonList(product2));
+
+        assert(validator.validate(person2).isEmpty());
+    }
+}
