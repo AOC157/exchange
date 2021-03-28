64307cf7441271f1f9aff5372fd2381f142f0418
Hatef Zamany <hatef.zamany@yahoo.com>
2021-03-28 20:58:14 +0430

    update exchange controller tests

--- src/test/java/com/example/exchange/controller/ExchangeControllerTest.java
+++ src/test/java/com/example/exchange/controller/ExchangeControllerTest.java
@@ -3,60 +3,70 @@ package com.example.exchange.controller;
 import com.example.exchange.model.Exchange;
 import com.example.exchange.model.Person;
 import com.example.exchange.model.Product;
-import com.example.exchange.service.ExchangeService;
+import com.example.exchange.repository.ExchangeRepository;
+import com.example.exchange.repository.PersonRepository;
+import com.example.exchange.repository.ProductRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasSize;
-import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
-@WebMvcTest(ExchangeController.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@Transactional
 public class ExchangeControllerTest {
 
-    @MockBean
-    private ExchangeService exchangeService;
+    private MockMvc mockMvc;
 
     @Autowired
-    private MockMvc mockMvc;
+    private WebApplicationContext applicationContext;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Autowired
+    private ExchangeRepository exchangeRepository;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders.webAppContextSetup(applicationContext).build();
+    }
 
     @Test
     public void insertTest() throws Exception {
-        Product product1 = new Product();
-        product1.setName("cake");
-        product1.setColor("brown");
-        product1.setPrice(5000.0);
+        Product savedProduct1 = productRepository.save(new Product("cake" ,"brown", 5000));
+        Product savedProduct2 = productRepository.save(new Product("pen" ,"red", 7000));
 
-        Product product2 = new Product();
-        product2.setName("pen");
-        product2.setColor("red");
-        product2.setPrice(7000.0);
-
-        Person person1 = new Person(Collections.singletonList(product1));
-        Person person2 = new Person(Collections.singletonList(product2));
+        Person person1 = personRepository.save(new Person(new ArrayList<>(Arrays.asList(savedProduct1,savedProduct2))));
+        Person person2 = personRepository.save(new Person(new ArrayList<>()));
 
         Exchange exchange = new Exchange(person2.getId(),person1.getId(),person1.getProducts());
 
         ObjectMapper mapper = new ObjectMapper();
         String jsonExchange = mapper.writeValueAsString(exchange);
-
-        given(exchangeService.save(any(Exchange.class))).willReturn(exchange);
 
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/exchange/insert")
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -65,61 +75,55 @@ public class ExchangeControllerTest {
                 .andExpect(status().isOk())
                 .andReturn();
 
-        Exchange resultExchange = new ObjectMapper().readValue(result.getResponse().getContentAsString(), Exchange.class);
+        Exchange savedExchange = new ObjectMapper().readValue(result.getResponse().getContentAsString(), Exchange.class);
 
-        assertEquals(exchange.getExchangedProducts().get(0).getName(),resultExchange.getExchangedProducts().get(0).getName());
+        assertThat(savedExchange.getExchangedProducts().get(0).getId()).isEqualTo(savedProduct1.getId());
+        assertThat(savedExchange.getExchangedProducts().get(0).getName()).isEqualTo(savedProduct1.getName());
+        assertThat(savedExchange.getExchangedProducts().get(0).getColor()).isEqualTo(savedProduct1.getColor());
+        assertThat(savedExchange.getExchangedProducts().get(0).getPrice()).isEqualTo(savedProduct1.getPrice());
+
+        assertThat(savedExchange.getExchangedProducts().get(1).getId()).isEqualTo(savedProduct2.getId());
+        assertThat(savedExchange.getExchangedProducts().get(1).getName()).isEqualTo(savedProduct2.getName());
+        assertThat(savedExchange.getExchangedProducts().get(1).getColor()).isEqualTo(savedProduct2.getColor());
+        assertThat(savedExchange.getExchangedProducts().get(1).getPrice()).isEqualTo(savedProduct2.getPrice());
     }
 
     @Test
     public void getTest() throws Exception {
-        Product product1 = new Product();
-        product1.setName("cake");
-        product1.setColor("brown");
-        product1.setPrice(5000.0);
+        Product savedProduct1 = productRepository.save(new Product("cookie" ,"brown", 5000));
+        Product savedProduct2 = productRepository.save(new Product("pencil" ,"black", 7000));
 
-        Product product2 = new Product();
-        product2.setName("pen");
-        product2.setColor("red");
-        product2.setPrice(7000.0);
+        Exchange savedExchange = exchangeRepository.save(new Exchange(1,2,new ArrayList<>(Arrays.asList(savedProduct1,savedProduct2))));
 
-        Person person1 = new Person(Collections.singletonList(product1));
-        Person person2 = new Person(Collections.singletonList(product2));
-
-        Exchange exchange = new Exchange(person2.getId(),person1.getId(),person1.getProducts());
-
-        given(exchangeService.get(exchange.getId())).willReturn(exchange);
-
-        mockMvc.perform(MockMvcRequestBuilders.get("/exchange/get/" + exchange.getId())
+        mockMvc.perform(MockMvcRequestBuilders.get("/exchange/get/" + savedExchange.getId())
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.exchangedProducts[0].name").value(exchange.getExchangedProducts().get(0).getName()));
+                .andExpect(jsonPath("$.exchangedProducts[0].id").value(savedExchange.getExchangedProducts().get(0).getId()))
+                .andExpect(jsonPath("$.exchangedProducts[0].name").value(savedExchange.getExchangedProducts().get(0).getName()))
+                .andExpect(jsonPath("$.exchangedProducts[0].color").value(savedExchange.getExchangedProducts().get(0).getColor()))
+                .andExpect(jsonPath("$.exchangedProducts[0].price").value(savedExchange.getExchangedProducts().get(0).getPrice()))
+                .andExpect(jsonPath("$.sellerId").value(savedExchange.getSellerId()))
+                .andExpect(jsonPath("$.buyerId").value(savedExchange.getBuyerId()));
     }
 
     @Test
     public void getAllTest() throws Exception {
-        Product product1 = new Product();
-        product1.setName("cake");
-        product1.setColor("brown");
-        product1.setPrice(5000.0);
 
-        Product product2 = new Product();
-        product2.setName("pen");
-        product2.setColor("red");
-        product2.setPrice(7000.0);
+        Product savedProduct1 = productRepository.save(new Product("ball" ,"yellow", 5000));
+        Product savedProduct2 = productRepository.save(new Product("oil" ,"yellow", 37000));
 
-        Person person1 = new Person(Collections.singletonList(product1));
-        Person person2 = new Person(Collections.singletonList(product2));
+        Exchange savedExchange = exchangeRepository.save(new Exchange(1,2,new ArrayList<>(Arrays.asList(savedProduct1,savedProduct2))));
 
-        Exchange exchange = new Exchange(person2.getId(),person1.getId(),person1.getProducts());
-
-        List<Exchange> allExchanges = Collections.singletonList(exchange);
-
-        given(exchangeService.getAll()).willReturn(allExchanges);
+        int size = exchangeRepository.findAll().size();
 
         mockMvc.perform(MockMvcRequestBuilders.get("/exchange/getAll")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$[0].exchangedProducts[0].name").value(exchange.getExchangedProducts().get(0).getName()));
+                .andExpect(jsonPath("$", hasSize(size)))
+                .andExpect(jsonPath("$["+ (size - 1) +"].exchangedProducts[0].name").value(savedExchange.getExchangedProducts().get(0).getName()))
+                .andExpect(jsonPath("$["+ (size - 1) +"].exchangedProducts[0].color").value(savedExchange.getExchangedProducts().get(0).getColor()))
+                .andExpect(jsonPath("$["+ (size - 1) +"].exchangedProducts[0].price").value(savedExchange.getExchangedProducts().get(0).getPrice()))
+                .andExpect(jsonPath("$["+ (size - 1) +"].sellerId").value(savedExchange.getSellerId()))
+                .andExpect(jsonPath("$["+ (size - 1) +"].buyerId").value(savedExchange.getBuyerId()));
     }
 }
